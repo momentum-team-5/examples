c0f29c342c73ad54efed04cad6c07bf974a39150
Amy Gori <amydgori@gmail.com>
2020-09-30 12:40:53 -0400

    Reorganize and clear input field

--- ajax-todo/main.js
+++ ajax-todo/main.js
@@ -1,50 +1,59 @@
 /* globals fetch, moment */
 const url = 'http://localhost:3000/todos'
+const todoList = document.querySelector('#todo-list')
 
 document.addEventListener('submit', function (event) {
   event.preventDefault()
   createTodo()
 })
 
+todoList.addEventListener('click', function (e) {
+  if (e.target.matches('.delete')) {
+    console.log(e.target.parentElement.dataset.id)
+    deleteTodo(e.target.parentElement.dataset.id)
+  }
+})
+
 function renderTodoList () {
   fetch(url)
     .then(res => res.json())
     .then(todoData => {
-      const todoList = document.querySelector('#todo-list')
-      for (const item of todoData) {
-        const todoItemEl = document.createElement('li')
-        todoItemEl.dataset.id = item.id
-        todoItemEl.innerText = item.todoItem
-        const deleteIcon = document.createElement('span')
-        deleteIcon.classList.add('fas', 'fa-times', 'mar-l-xs', 'delete')
-        todoItemEl.appendChild(deleteIcon)
-        todoList.appendChild(todoItemEl)
+      for (const todo of todoData) {
+        renderTodoItem(todo)
       }
     })
 }
 
-function createTodo () {
-  const todoInput = document.querySelector('#todo-input').value
-  console.log(todoInput)
+function renderTodoItem (todo) {
+  const todoList = document.querySelector('#todo-list')
+  const todoItemEl = document.createElement('li')
+  todoItemEl.dataset.id = todo.id
+  todoItemEl.innerText = todo.todoItem
+  const deleteIcon = document.createElement('span')
+  deleteIcon.classList.add('fas', 'fa-times', 'mar-l-xs', 'delete')
+  todoItemEl.appendChild(deleteIcon)
+  todoList.appendChild(todoItemEl)
+}
 
-  fetch(url, {
+function createTodo () {
+  const todoInputField = document.querySelector('#todo-input')
+
+  const requestData = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      todoItem: todoInput,
+      todoItem: todoInputField.value,
       created_at: moment().format()
     })
-  })
+  }
+
+  fetch(url, requestData)
     .then(res => res.json())
     .then(data => {
-      const todoList = document.querySelector('#todo-list')
-      const todoItemEl = document.createElement('li')
-      todoItemEl.innerText = data.todoItem
-      todoList.appendChild(todoItemEl)
+      todoInputField.value = ''
+      renderTodoItem(data)
     })
 }
-
-renderTodoList()
 
 function deleteTodo (todoId) {
   fetch(url + '/' + todoId, {
@@ -57,11 +66,4 @@ function deleteTodo (todoId) {
     })
 }
 
-const todoList = document.querySelector('#todo-list')
-
-todoList.addEventListener('click', function (e) {
-  if (e.target.matches('.delete')) {
-    console.log(e.target.parentElement.dataset.id)
-    deleteTodo(e.target.parentElement.dataset.id)
-  }
-})
+renderTodoList()
