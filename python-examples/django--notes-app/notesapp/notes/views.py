c7d4e4a98d638b0848a55c13297e778c64523345
Will Jukes <william.r.jukes@gmail.com>
2020-10-19 15:46:41 -0400

    trying to remove git repo

---
+++ python-examples/django--notes-app/notesapp/notes/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Note
+
+# Create your views here.
+def notes_list(request):
+    pass
+
+
+def notes_detail(request, pk):
+    pass
